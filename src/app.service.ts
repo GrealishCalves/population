f2ae7f463e831145016e849a206f87f49d3788e0
maxim <maximmilshtien@gmail.com>
2023-08-19 18:57:26 +0300

    add spans

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
+import { Span } from '@metinseylan/nestjs-opentelemetry';
 
 export type Nation = {
   data: data[];
@@ -16,24 +17,25 @@ export type data = {
 @Injectable()
 export class InvokeDataService {
   constructor(private httpService: HttpService) {}
-
+  @Span()
   public getPopulation(obj: any): Promise<Nation> {
     const year = this.getYear(obj);
     const population = this.getPopulationData(year);
     return population;
   }
-
+  @Span()
   private getYear(obj: any): string {
     const year = obj.year;
     return year;
   }
-
+  @Span()
   private async getPopulationData(year): Promise<Nation> {
     const response = await lastValueFrom(
       this.httpService.get(
         `https://datausa.io/api/data?drilldowns=Nation&measures=Population&year=${year}`,
       ),
     );
+    console.log(response.data);
     return response.data;
   }
 }
